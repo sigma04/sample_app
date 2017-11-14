59d2620d234e0a001f62e33ffcb2669cf68b2dad
sigma4 <gtwangw@163.com>
2017-11-14 15:09:09 +0800

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
+
   root 'application#hello'
-=======
+
   get 'static_pages/home'
 
   get 'static_pages/help'
@@ -9,6 +9,5 @@ Rails.application.routes.draw do
   get 'static_pages/about'
 
   root 'static_pages#home'
->>>>>>> sample_app_dev
 
 end
