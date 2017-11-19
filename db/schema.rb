fd2eb606656f3b93728622ca7fdf24483f58fd29
sigma4 <gtwangw@163.com>
2017-11-19 09:39:09 +0800

    代码清单6.10

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171118043007) do
+ActiveRecord::Schema.define(version: 20171119012539) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
