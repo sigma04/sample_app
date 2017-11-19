4769729c2ddf830baeb79b3a61042379867798ec
sigma4 <gtwangw@163.com>
2017-11-19 10:42:57 +0800

    代码清单6.43

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -41,4 +41,14 @@ duplicate_user.email = @user.email.upcase
 assert_not duplicate_user.valid?
 end
 
+test "password should be present (nonblank)" do
+@user.password = @user.password_confirmation = " " * 6
+assert_not @user.valid?
+end
+
+test "password should have a minimum length" do
+@user.password = @user.password_confirmation = "a" * 5
+assert_not @user.valid?
+end
+
 end
